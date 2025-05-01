11d3aad223a9d0d677a25cb4cc814d2521b8ccce
Amani Abdullah <amaniabdallah200@gmail.com>
2025-05-01 11:49:25 +0300

    Add CORS headers to allow frontend API requests

--- livo/admin.py
+++ livo/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Role, Task, Achievement
 
 # Register your models here.
+admin.site.register(Role)
+admin.site.register(Task)
+admin.site.register(Achievement)
